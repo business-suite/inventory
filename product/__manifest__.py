16b03942bbde805313a8b6baf77691679cd9bff5
petr-nguyen <petr.nguyen@gusrix.com>
2022-10-23 00:18:51 +0700

    update menu

--- product/__manifest__.py
+++ product/__manifest__.py
@@ -32,7 +32,7 @@ Print product labels with barcode.
         'security/ir.model.access.csv',
         'wizard/product_label_layout_views.xml',
         'views/product_attribute_views.xml',
-        'views/product_views.xml',
+        'views/product_views2.xml',
         'views/product_template_views.xml',
         'views/product_pricelist_views.xml',
         'views/res_partner_views.xml',
